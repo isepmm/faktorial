d9ecf47eba0e038f7885e0c90a01c166a1643e0f
isepmm <isepmm@gmail.com>
2017-04-08 12:15:03 +0700

    Add files via upload

---
+++ belah_ketupat/src/belah_ketupat.java
@@ -0,0 +1,33 @@
+public class belah_ketupat {
+
+  public static void main(String[] args) {
+
+
+    for (int i=1;i<=4;i++){
+             for(int j=4;j>=i;j--){
+                 System.out.print(" ");
+             }
+             for(int k=1;k<=i;k++){
+                 System.out.print("*");
+             }
+             for (int l=1;l<=i-1;l++){
+                 System.out.print("*");
+             }
+             System.out.println();
+         }
+   for (int i=1;i<=5;i++){
+             for(int j=1;j<=i-1;j++){
+                 System.out.print(" ");
+             }
+             for(int z=5;z>=i;z--){
+              System.out.print("*");
+              }
+             for(int k=4;k>=i;k--){
+               System.out.print("*");
+               }
+             System.out.println("");
+         }
+
+  }
+
+}
